a4fd531bfb1fc0ddd8f8a477c37929d403b61fd9
jayabharathi sankarappan <jayabharathi_san@yahoo.com>
2019-11-09 22:41:31 +1100

    single menu bar -site Master -Jaya

--- testmaster2/Site.Master.designer.cs
+++ testmaster2/Site.Master.designer.cs
@@ -22,15 +22,6 @@ namespace testmaster2 {
         protected global::System.Web.UI.WebControls.Menu DICTMenuBar1;
         
         /// <summary>
-        /// DICTMenuBar2 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Menu DICTMenuBar2;
-        
-        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
