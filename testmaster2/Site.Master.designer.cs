427f2d985eaefbce3f8e8394d978d8891ad89fd5
jayabharathi sankarappan <jayabharathi_san@yahoo.com>
2019-11-11 15:52:25 +1100

    master page selection highlight

--- testmaster2/Site.Master.designer.cs
+++ testmaster2/Site.Master.designer.cs
@@ -29,5 +29,14 @@ namespace testmaster2 {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder MainContent;
+        
+        /// <summary>
+        /// Label1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
     }
 }
